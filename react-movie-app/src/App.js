bf6213143fa73e609caa3eec3d3e18fe1146712f
ThaiThuan <s3877024@rmit.edu.vn>
2022-08-27 10:54:59 +0700

    add css for uploadpage

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,30 +1,30 @@
 import React, { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
-import LogIn from './components/auth/LogIn';
-import SignUp from './components/auth/SignUp';
-import FinishedSignUp from './components/auth/finishedSignUp';
+import LogIn from "./components/auth/LogIn";
+import SignUp from "./components/auth/SignUp";
+import FinishedSignUp from "./components/auth/finishedSignUp";
 import Banner from "./components/banner/Banner";
 import Main from "./components/layout/Main";
 import HomePage from "./pages/HomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import MoviePage from "./pages/MoviePage";
-import { MovieUpload } from "./components/auth/MovieUpload";
+import UploadPage from "./pages/UploadPage";
+
 //https://api.themoviedb.org/3/movie/now_playing?api_key=<<744d8cc0b08621bc75b1dbffe80f9214>>&language=en-US&page=1
 const App = () => {
     return (
         <Fragment>
             <Routes>
-                <Route path="/upload" element={<MovieUpload/>}/>
-                <Route path="/login" element={<LogIn/>}/>
-                <Route path="/signUp" element={<SignUp/>}/>
-                <Route path="/finishedSignUp" element={<FinishedSignUp/>}/>
+                <Route path="/upload" element={<UploadPage />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/finishedSignUp" element={<FinishedSignUp />} />
                 <Route element={<Main></Main>}>
                     <Route
                         path="/"
                         element={
                             <>
-                                
                                 <HomePage></HomePage>
                             </>
                         }></Route>
